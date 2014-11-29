ab46ff0231a7308a5a966fc51809f057f7711515
qinyu <qinyusuain@gmail.com>
2014-11-30 00:48:56 +0800

    add mock services

--- features/step_definitions/calabash_steps.rb
+++ features/step_definitions/calabash_steps.rb
@@ -7,4 +7,6 @@ end
 Then(/^I scroll down to the list bottom$/) do
   #pan_up()
   scroll("android.support.v7.widget.RecyclerView id:'book_list'", :down)
+  scroll("android.support.v7.widget.RecyclerView id:'book_list'", :down)
+  scroll("android.support.v7.widget.RecyclerView id:'book_list'", :down)
 end
