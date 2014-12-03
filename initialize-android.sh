2a4df276de4d4ecbdf62e6b243f69c2eca0dba48
qinyu <qinyusuain@gmail.com>
2014-12-03 13:34:26 +0800

    Update initialize-android.sh

--- initialize-android.sh
+++ initialize-android.sh
@@ -10,8 +10,8 @@ if [ ! -e ${INITIALIZATION_FILE} ]; then
   # fetch and initialize $ANDROID_HOME
   download-android
   # Use the latest android sdk tools
-  echo y | android update sdk --no-ui --filter platform-tools > /dev/null
-  echo y | android update sdk --no-ui --filter tools > /dev/null
+  echo y | android update sdk --no-ui --filter platform-tool > /dev/null
+  echo y | android update sdk --no-ui --filter tool > /dev/null
 
   # The BuildTools version used by your project
   echo y | android update sdk --no-ui --filter build-tools-21.1.1 --all > /dev/null
@@ -20,11 +20,11 @@ if [ ! -e ${INITIALIZATION_FILE} ]; then
   echo y | android update sdk --no-ui --filter android-21> /dev/null
 
   # uncomment to install the Extra/Android Support Library
-  echo y | android update sdk --no-ui --filter extra-android-support --all > /dev/null
+  echo y | android update sdk --no-ui --filter extra --all > /dev/null
 
   # uncomment these if you are using maven/gradle to build your android project
-  echo y | android update sdk --no-ui --filter extra-google-m2repository --all > /dev/null
-  echo y | android update sdk --no-ui --filter extra-android-m2repository --all > /dev/null
+  # echo y | android update sdk --no-ui --filter extra-google-m2repository --all > /dev/null
+  # echo y | android update sdk --no-ui --filter extra-android-m2repository --all > /dev/null
 
   # Specify at least one system image if you want to run emulator tests
   echo y | android update sdk --no-ui --filter sys-img-armeabi-v7a-android-21 --all > /dev/null
