614611bddef4bf13ad685b1804962733da018de0
qinyu <qinyusuain@gmail.com>
2014-12-04 12:47:51 +0800

    Update android-wait-for-emualtor.sh
    increase check interval

--- snap-ci/android-wait-for-emualtor.sh
+++ snap-ci/android-wait-for-emualtor.sh
@@ -11,11 +11,11 @@ until [[ "$bootanim" =~ "stopped" ]]; do
    echo "$bootanim"
    if [[ "$bootanim" =~ "not found" ]]; then
       let "failcounter += 1"
-      if [[ $failcounter -gt 15 ]]; then
+      if [[ $failcounter -gt 2 ]]; then
         echo "Failed to start emulator"
         exit 1
       fi
    fi
-   sleep 1
+   sleep 15
 done
 echo "Done"
