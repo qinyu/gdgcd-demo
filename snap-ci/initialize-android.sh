0eca166fedad0a8f62b30ce09eaac20b35e34e11
qinyu <qinyusuain@gmail.com>
2016-05-22 17:10:20 +0800

    fix snap-ci and circleci missing sdk components

--- snap-ci/initialize-android.sh
+++ snap-ci/initialize-android.sh
@@ -10,25 +10,10 @@ if [ ! -e ${INITIALIZATION_FILE} ]; then
   # fetch and initialize $ANDROID_HOME
   download-android
   # Use the latest android sdk tools
-  echo y | android update sdk --no-ui --filter platform-tool > /dev/null
-  echo y | android update sdk --no-ui --filter tool > /dev/null
-
-  # The BuildTools version used by your project
-  echo y | android update sdk --no-ui --filter build-tools-23.0.3 --all > /dev/null
-
-
-  # The SDK version used to compile your project
-  echo y | android update sdk --no-ui --filter android-23> /dev/null
-
-  # uncomment to install the Extra/Android Support Library
-  echo y | android update sdk --no-ui --filter extra --all > /dev/null
-
-  # uncomment these if you are using maven/gradle to build your android project
-  # echo y | android update sdk --no-ui --filter extra-google-m2repository --all > /dev/null
-  echo y | android update sdk --no-ui --filter extra-android-m2repository --all > /dev/null
+  - echo y | android update sdk --no-ui --all -s --filter tools-preview,platform-tools-preview,build-tools-24.0.0-preview,android-N,extra-android-support,extra-android-m2repository
 
   # Specify at least one system image if you want to run emulator tests
-  echo y | android update sdk --no-ui --filter sys-img-armeabi-v7a-android-21 --all > /dev/null
-  
+  # echo y | android update sdk --no-ui --filter sys-img-armeabi-v7a-android-21 --all > /dev/null
+
   touch ${INITIALIZATION_FILE}
 fi
