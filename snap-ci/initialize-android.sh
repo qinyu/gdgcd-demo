c037c8b7bd7771c6b497c81cce03f7014b3654bc
qinyu <qinyusuain@gmail.com>
2016-05-22 17:17:55 +0800

    seperate sdk components update

--- snap-ci/initialize-android.sh
+++ snap-ci/initialize-android.sh
@@ -10,7 +10,9 @@ if [ ! -e ${INITIALIZATION_FILE} ]; then
   # fetch and initialize $ANDROID_HOME
   download-android
   # Use the latest android sdk tools
-  - echo y | android update sdk --no-ui --all -s --filter tools-preview,platform-tools-preview,build-tools-24.0.0-preview,android-N,extra-android-support,extra-android-m2repository
+  - echo y | android update sdk --no-ui --all -s --filter tools-preview
+  - echo y | android update sdk --no-ui --all -s --filter platform-tools-preview,build-tools-24.0.0-preview,android-N
+  - echo y | android update sdk --no-ui --all -s --filter extra-android-support,extra-android-m2repository
 
   # Specify at least one system image if you want to run emulator tests
   # echo y | android update sdk --no-ui --filter sys-img-armeabi-v7a-android-21 --all > /dev/null
