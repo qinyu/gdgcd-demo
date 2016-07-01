3ee5e3bc244ce6a444a7e1f7805e4f6910ed129d
qinyu <qinyusuain@gmail.com>
2016-07-01 13:23:59 +0800

    update tools version

--- app/src/androidTest/java/org/gdgcd/demo/ApplicationTest.java
+++ app/src/androidTest/java/org/gdgcd/demo/ApplicationTest.java
@@ -1,17 +1,17 @@
 package org.gdgcd.demo;
 
 import android.app.Application;
+import android.support.test.runner.AndroidJUnit4;
 import android.test.ApplicationTestCase;
 
-/**
- * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
- */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
-        super(Application.class);
-    }
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
+
+@RunWith(AndroidJUnit4.class)
+public class ApplicationTest {
+
+    @Test
     public void testName() throws Exception {
-        fail();
     }
 }
